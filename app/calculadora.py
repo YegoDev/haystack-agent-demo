ebbdf79e40102b0551dae758162b7b4585b9bce6
YegoDev <jeisson46@hotmail.com>
2026-03-22 11:32:16 -0500

    Para pruebas de usuario, volvemos a dejar el error en el divideo de calculadora.py

--- app/calculadora.py
+++ app/calculadora.py
@@ -8,6 +8,4 @@ def multiply(a, b):
     return a * b
 
 def divide(a, b):
-    if b == 0:
-        return 'Error: División por cero'
-    return a / b
+    return a / b  # ⚠️ Bug intencional: no maneja división por cero
